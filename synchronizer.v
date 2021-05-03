935b5cf8843e23ba5fe0a456848ee8d7e7a2249d
Vladislav Rykov <vladoss@list.ru>
2021-05-02 17:46:09 -0700

    synchronizer & DFF1 split

--- synchronizer.v
+++ synchronizer.v
@@ -1,28 +1,7 @@
-// DFF1
-// Author: Vikas Gundapuneedi
-
-module DFF1 #(parameter PTR_SZ=2)
-             (input  rst,
-              input  clk,
-              input  [PTR_SZ:0] addr_gray,
-              output reg [PTR_SZ:0] rq2_addr, rq1_addr
-);
- 
- //synchronizing the read pointer into the write clock domain 
-
-  always @(posedge clk or negedge rst)
-  begin
-    if (!rst) begin
-      { rq2_addr, rq1_addr } <= 2'b0;
-    end else begin 
-      { rq2_addr, rq1_addr } <= { rq1_addr, addr_gray };
-    end
-  end
-
-endmodule
-
 // synchronizer
 // Author: Vladislav Rykov
+`include "DFF1.v"
+
 module synchronizer #(parameter PTR_SZ=2)
                      (input clk1, clk2, rst,
                       input [PTR_SZ:0] waddr_gray, raddr_gray,
