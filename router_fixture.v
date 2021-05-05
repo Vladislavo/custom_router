4a16ff93ec1309758f244bd73cad53cf258093a3
Vladislav Rykov <vladoss@list.ru>
2021-05-05 04:15:28 -0700

    WORKING & SYNTHESIZABLE ROUTER

--- router_fixture.v
+++ router_fixture.v
@@ -3,8 +3,8 @@
 
 `include "router.v"
 
-`define CLK_1     2 // F=250KHz T=4us
-`define CLK_2     5 // F=100KHz T=10us
+`define CLK_1     4 // F=250KHz T=4us
+`define CLK_2     10 // F=100KHz T=10us
 
 `define DEPTH     4
 `define WIDTH     11
@@ -192,142 +192,18 @@ module router_fixture;
        #(`CLK_1*2) packet_in = crc; // crc
 
        #(`CLK_1*2) $display("=======================================Packet 12 received -> Port 2");
-                    packet_in = 1; crc = ^packet_in; // source_id
-       #(`CLK_1*2) packet_in = 250; crc = (^packet_in)^crc; // dest_id port 3
-       #(`CLK_1*2) packet_in = 6; crc = (^packet_in)^crc; // size
-       #(`CLK_1*2) packet_in = 0; crc = (^packet_in)^crc; // data 1
-       #(`CLK_1*2) packet_in = 1; crc = (^packet_in)^crc; // data 2
-       #(`CLK_1*2) packet_in = 2; crc = (^packet_in)^crc; // data 3
-       #(`CLK_1*2) packet_in = 3; crc = (^packet_in)^crc; // data 4
-       #(`CLK_1*2) packet_in = 4; crc = (^packet_in)^crc; // data 5
-       #(`CLK_1*2) packet_in = 5; crc = (^packet_in)^crc; // data 5
-       #(`CLK_1*2) packet_in = crc;// crc
        
-       #(`CLK_1*2) $display("=======================================Packet 13 received -> Port 3");
-       #(`CLK_1*2) packet_valid_i = 1; packet_in = 1; crc = ^packet_in; // source_id
-       #(`CLK_1*2) packet_in = 25; crc = (^packet_in)^crc; // dest_id port 1
-       #(`CLK_1*2) packet_in = 4; crc = (^packet_in)^crc; // size
-       #(`CLK_1*2) packet_in = 0; crc = (^packet_in)^crc; // data 1
-       #(`CLK_1*2) packet_in = 1; crc = (^packet_in)^crc; // data 2
-       #(`CLK_1*2) packet_in = 2; crc = (^packet_in)^crc; // data 3
-       #(`CLK_1*2) packet_in = 3; crc = (^packet_in)^crc; // data 4
-       #(`CLK_1*2) packet_in = crc; // crc
-       
-       
-       #(`CLK_1*2) $display("=======================================Packet 14 received -> Port 1");
-                    packet_in = 2; crc = ^packet_in; // source_id
-       #(`CLK_1*2) packet_in = 150; crc = (^packet_in)^crc; // dest_id port 2
-       #(`CLK_1*2) packet_in = 5; crc = (^packet_in)^crc; // size
-       #(`CLK_1*2) packet_in = 0; crc = (^packet_in)^crc; // data 1
-       #(`CLK_1*2) packet_in = 1; crc = (^packet_in)^crc; // data 2
-       #(`CLK_1*2) packet_in = 2; crc = (^packet_in)^crc; // data 3
-       #(`CLK_1*2) packet_in = 3; crc = (^packet_in)^crc; // data 4
-       #(`CLK_1*2) packet_in = 4; crc = (^packet_in)^crc; // data 5
-       #(`CLK_1*2) packet_in = crc; // crc
-
-       #(`CLK_1*2) $display("=======================================Packet 15 received -> Port 2");
-                    packet_in = 1; crc = ^packet_in; // source_id
-       #(`CLK_1*2) packet_in = 250; crc = (^packet_in)^crc; // dest_id port 3
-       #(`CLK_1*2) packet_in = 6; crc = (^packet_in)^crc; // size
-       #(`CLK_1*2) packet_in = 0; crc = (^packet_in)^crc; // data 1
-       #(`CLK_1*2) packet_in = 1; crc = (^packet_in)^crc; // data 2
-       #(`CLK_1*2) packet_in = 2; crc = (^packet_in)^crc; // data 3
-       #(`CLK_1*2) packet_in = 3; crc = (^packet_in)^crc; // data 4
-       #(`CLK_1*2) packet_in = 4; crc = (^packet_in)^crc; // data 5
-       #(`CLK_1*2) packet_in = 5; crc = (^packet_in)^crc; // data 5
-       #(`CLK_1*2) packet_in = crc;// crc
-       
-       #(`CLK_1*2) $display("=======================================Packet 16 received -> Port 3");
-       #(`CLK_1*2) packet_valid_i = 1; packet_in = 1; crc = ^packet_in; // source_id
-       #(`CLK_1*2) packet_in = 25; crc = (^packet_in)^crc; // dest_id port 1
-       #(`CLK_1*2) packet_in = 4; crc = (^packet_in)^crc; // size
-       #(`CLK_1*2) packet_in = 0; crc = (^packet_in)^crc; // data 1
-       #(`CLK_1*2) packet_in = 1; crc = (^packet_in)^crc; // data 2
-       #(`CLK_1*2) packet_in = 2; crc = (^packet_in)^crc; // data 3
-       #(`CLK_1*2) packet_in = 3; crc = (^packet_in)^crc; // data 4
-       #(`CLK_1*2) packet_in = crc; // crc
-       
-       
-       #(`CLK_1*2) $display("=======================================Packet 17 received -> Port 1");
-                    packet_in = 2; crc = ^packet_in; // source_id
-       #(`CLK_1*2) packet_in = 150; crc = (^packet_in)^crc; // dest_id port 2
-       #(`CLK_1*2) packet_in = 5; crc = (^packet_in)^crc; // size
-       #(`CLK_1*2) packet_in = 0; crc = (^packet_in)^crc; // data 1
-       #(`CLK_1*2) packet_in = 1; crc = (^packet_in)^crc; // data 2
-       #(`CLK_1*2) packet_in = 2; crc = (^packet_in)^crc; // data 3
-       #(`CLK_1*2) packet_in = 3; crc = (^packet_in)^crc; // data 4
-       #(`CLK_1*2) packet_in = 4; crc = (^packet_in)^crc; // data 5
-       #(`CLK_1*2) packet_in = crc; // crc
-
-       #(`CLK_1*2) $display("=======================================Packet 18 received -> Port 2");
-                    packet_in = 1; crc = ^packet_in; // source_id
-       #(`CLK_1*2) packet_in = 250; crc = (^packet_in)^crc; // dest_id port 3
-       #(`CLK_1*2) packet_in = 6; crc = (^packet_in)^crc; // size
-       #(`CLK_1*2) packet_in = 0; crc = (^packet_in)^crc; // data 1
-       #(`CLK_1*2) packet_in = 1; crc = (^packet_in)^crc; // data 2
-       #(`CLK_1*2) packet_in = 2; crc = (^packet_in)^crc; // data 3
-       #(`CLK_1*2) packet_in = 3; crc = (^packet_in)^crc; // data 4
-       #(`CLK_1*2) packet_in = 4; crc = (^packet_in)^crc; // data 5
-       #(`CLK_1*2) packet_in = 5; crc = (^packet_in)^crc; // data 5
-       #(`CLK_1*2) packet_in = crc;// crc
-       
-       #(`CLK_1*2) $display("=======================================Packet 19 received -> Port 3");
-       #(`CLK_1*2) packet_valid_i = 1; packet_in = 1; crc = ^packet_in; // source_id
-       #(`CLK_1*2) packet_in = 25; crc = (^packet_in)^crc; // dest_id port 1
-       #(`CLK_1*2) packet_in = 4; crc = (^packet_in)^crc; // size
-       #(`CLK_1*2) packet_in = 0; crc = (^packet_in)^crc; // data 1
-       #(`CLK_1*2) packet_in = 1; crc = (^packet_in)^crc; // data 2
-       #(`CLK_1*2) packet_in = 2; crc = (^packet_in)^crc; // data 3
-       #(`CLK_1*2) packet_in = 3; crc = (^packet_in)^crc; // data 4
-       #(`CLK_1*2) packet_in = crc; // crc
-       
-       
-       #(`CLK_1*2) $display("=======================================Packet 20 received -> Port 1");
-                    packet_in = 2; crc = ^packet_in; // source_id
-       #(`CLK_1*2) packet_in = 150; crc = (^packet_in)^crc; // dest_id port 2
-       #(`CLK_1*2) packet_in = 5; crc = (^packet_in)^crc; // size
-       #(`CLK_1*2) packet_in = 0; crc = (^packet_in)^crc; // data 1
-       #(`CLK_1*2) packet_in = 1; crc = (^packet_in)^crc; // data 2
-       #(`CLK_1*2) packet_in = 2; crc = (^packet_in)^crc; // data 3
-       #(`CLK_1*2) packet_in = 3; crc = (^packet_in)^crc; // data 4
-       #(`CLK_1*2) packet_in = 4; crc = (^packet_in)^crc; // data 5
-       #(`CLK_1*2) packet_in = crc; // crc
-       
-       #(`CLK_1*2) $display("=======================================Packet 21 received -> Port 1");
-                    packet_in = 2; crc = ^packet_in; // source_id
-       #(`CLK_1*2) packet_in = 150; crc = (^packet_in)^crc; // dest_id port 2
-       #(`CLK_1*2) packet_in = 5; crc = (^packet_in)^crc; // size
-       #(`CLK_1*2) packet_in = 0; crc = (^packet_in)^crc; // data 1
-       #(`CLK_1*2) packet_in = 1; crc = (^packet_in)^crc; // data 2
-       #(`CLK_1*2) packet_in = 2; crc = (^packet_in)^crc; // data 3
-       #(`CLK_1*2) packet_in = 3; crc = (^packet_in)^crc; // data 4
-       #(`CLK_1*2) packet_in = 4; crc = (^packet_in)^crc; // data 5
-       #(`CLK_1*2) packet_in = crc; // crc
-
-       #(`CLK_1*2) $display("=======================================Packet 22 received -> Port 1");
-                    packet_in = 2; crc = ^packet_in; // source_id
-       #(`CLK_1*2) packet_in = 150; crc = (^packet_in)^crc; // dest_id port 2
-       #(`CLK_1*2) packet_in = 5; crc = (^packet_in)^crc; // size
-       #(`CLK_1*2) packet_in = 0; crc = (^packet_in)^crc; // data 1
-       #(`CLK_1*2) packet_in = 1; crc = (^packet_in)^crc; // data 2
-       #(`CLK_1*2) packet_in = 2; crc = (^packet_in)^crc; // data 3
-       #(`CLK_1*2) packet_in = 3; crc = (^packet_in)^crc; // data 4
-       #(`CLK_1*2) packet_in = 4; crc = (^packet_in)^crc; // data 5
-       #(`CLK_1*2) packet_in = crc; // crc
-
        #(`CLK_1*2) packet_valid_i = 0;
-
-
      end
      begin: checking_thread
-       for (j = 0; j < 143; j = j+1) begin
+       for (j = 0; j < 100; j = j+1) begin
          // 1. memory dump
          #(`CLK_2) $display("FIFO1 T=%3d", $time); for (i = 0; i < 11; i = i+1) 
-                       $display("[0][%d] = %b [1][%d] = %b [2][%d] = %b [3][%d] = %b", i, r.fifo1.fm.memory[0][i], i, r.fifo1.fm.memory[1][i], i, r.fifo1.fm.memory[2][i], i, r.fifo1.fm.memory[3][i]);
+                       $display("[0][%d] = %3d [1][%d] = %3d [2][%d] = %3d [3][%d] = %3d", i, r.fifo1.fm.memory[0][i], i, r.fifo1.fm.memory[1][i], i, r.fifo1.fm.memory[2][i], i, r.fifo1.fm.memory[3][i]);
                    $display("FIFO 2"); for (i = 0; i < 11; i = i+1) 
-                       $display("[0][%d] = %b [1][%d] = %b [2][%d] = %b [3][%d] = %b", i, r.fifo2.fm.memory[0][i], i, r.fifo2.fm.memory[1][i], i, r.fifo2.fm.memory[2][i], i, r.fifo2.fm.memory[3][i]);
+                       $display("[0][%d] = %3d [1][%d] = %3d [2][%d] = %3d [3][%d] = %3d", i, r.fifo2.fm.memory[0][i], i, r.fifo2.fm.memory[1][i], i, r.fifo2.fm.memory[2][i], i, r.fifo2.fm.memory[3][i]);
                    $display("FIFO 3"); for (i = 0; i < 11; i = i+1) 
-                       $display("[0][%d] = %b [1][%d] = %b [2][%d] = %b [3][%d] = %b", i, r.fifo3.fm.memory[0][i], i, r.fifo3.fm.memory[1][i], i, r.fifo3.fm.memory[2][i], i, r.fifo3.fm.memory[3][i]);
+                       $display("[0][%d] = %3d [1][%d] = %3d [2][%d] = %3d [3][%d] = %3d", i, r.fifo3.fm.memory[0][i], i, r.fifo3.fm.memory[1][i], i, r.fifo3.fm.memory[2][i], i, r.fifo3.fm.memory[3][i]);
          
        end
      end
@@ -335,13 +211,14 @@ module router_fixture;
        $vcdpluson;
      end
      begin: ending_thread
-       #(143*`CLK_2) disable clock_1_thread; disable clock_2_thread; disable stimulus_thread; disable checking_thread; disable dve_thread;
+       #(100*`CLK_2) disable clock_1_thread; disable clock_2_thread; disable stimulus_thread; disable checking_thread; disable dve_thread;
      end
    join
    $finish;
   end
   
   initial
-    $monitor("T=%3d, clk1 = %d, clk2 = %d, rst = %d, pin = %d, pvi = %d, sps=%d, pvo1 = %d, pout1 = %d, pvo2 = %d, pout2 = %d, pvo3 = %d, pout3 = %d, f1.waddr = %d f1.raddr = %d, f1.wfull = %d, f2.waddr = %d f2.raddr = %d, f2.wfull = %d, f3.waddr = %d f3.raddr = %d, f3.wfull = %d, f3.cs = %d, f3.temp3 = %d, dest_p = %d", $time, clk1, clk2, rst, packet_in, packet_valid_i, stop_packet_send, packet_valid_o1, packet_out1, packet_valid_o2, packet_out2, packet_valid_o3, packet_out3, r.fifo1.fwl.waddr, r.fifo1.frl.raddr, r.fifo1.wfull, r.fifo2.fwl.waddr, r.fifo2.frl.raddr, r.fifo2.wfull, r.fifo3.fwl.waddr, r.fifo3.frl.raddr, r.fifo3.wfull, r.packet_receiver1.present_state, r.packet_receiver1.temp3, r.packet_receiver1.dest_p);
+    $monitor("T=%3d, clk1 = %d, clk2 = %d, rst = %d, pin = %d, pvi = %d, sps = %d, pvo1 = %d, pout1 = %d, pvo2 = %d, pout2 = %d, pvo3 = %d, pout3 = %d", $time, clk1, clk2, rst, packet_in, packet_valid_i, stop_packet_send, packet_valid_o1, packet_out1, packet_valid_o2, packet_out2, packet_valid_o3, packet_out3);
+    //$monitor("T=%3d, clk1 = %d, clk2 = %d, rst = %d, pin = %d, pvi = %d, sps=%d, pvo1 = %d, pout1 = %d, pvo2 = %d, pout2 = %d, pvo3 = %d, pout3 = %d, f1.waddr = %d f1.raddr = %d, f1.wfull = %d, f2.waddr = %d f2.raddr = %d, f2.wfull = %d, f3.waddr = %d f3.raddr = %d, f3.wfull = %d, f3.cs = %d, f3.temp3 = %d, dest_p = %d", $time, clk1, clk2, rst, packet_in, packet_valid_i, stop_packet_send, packet_valid_o1, packet_out1, packet_valid_o2, packet_out2, packet_valid_o3, packet_out3, r.fifo1.fwl.waddr, r.fifo1.frl.raddr, r.fifo1.wfull, r.fifo2.fwl.waddr, r.fifo2.frl.raddr, r.fifo2.wfull, r.fifo3.fwl.waddr, r.fifo3.frl.raddr, r.fifo3.wfull, r.packet_receiver1.present_state, r.packet_receiver1.temp3, r.packet_receiver1.dest_p);
 
 endmodule
